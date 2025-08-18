4627170e8f5ed7a734a559ea1e9b262dd967be48
hgh3k <harryh2k@gmail.com>
2025-08-18 09:11:50 +0900

    Revert "Merge branch 'main' into hgh"
    This reverts commit 6a3aad5fcaebbabdad124af072d0f3a2b41c0894, reversing
    changes made to a3a172f010c58ceda7dee410bf40243c0c4146a0.

--- Source/LucioBall/Public/OSC/BouncyBall.h
+++ Source/LucioBall/Public/OSC/BouncyBall.h
@@ -6,7 +6,6 @@
 #include "GameFramework/Actor.h"
 #include "BouncyBall.generated.h"
 
-class USphereComponent;
 UCLASS()
 class LUCIOBALL_API ABouncyBall : public AActor
 {
@@ -16,52 +15,32 @@ public:
 	// Sets default values for this actor's properties
 	ABouncyBall();
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UStaticMeshComponent> BallMesh;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TObjectPtr<USphereComponent> Sphere;
 
 protected:
 	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BouncyBall Physics")
 	float Elasticity = 0.9f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float Friction = 0.35f;
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	FVector CurrentVelocity;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	FVector AngularVelocity;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float Radius = 100.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float GravityScale = 1.0f;
-	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<AActor> LastAttacker;
-	
 
 	UFUNCTION()
-	void OnBouncyBallHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnBallHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
-	FVector CalculateReflectionAndFriction(const FVector& InVelocity, const FHitResult& Hit);
-	bool CheckGrounded();
+	FVector PreviousVelocity;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "BouncyBall")
 	void BouncyBallAddImpulse(FVector Impulse, AActor* Attacker);
 
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "BouncyBall")
 	AActor* GetLastAttacker() const;
 };
